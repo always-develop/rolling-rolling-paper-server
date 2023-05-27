8839e4a553a28e51b015afb3bfa67608080dccd5
kkkkkksssssaaaa <kofc312@naver.com>
2023-05-27 23:44:35 +0900

    docs: add global annotations to resource handler class

--- src/common/resources/resource.handler.ts
+++ src/common/resources/resource.handler.ts
@@ -1,3 +1,11 @@
+/**
+ * 정적 파일에 접근하기 위한 핸들러
+ * 모든 모든 정적 파일은 <rootDir>/common/resources 의 하위 디렉토리에 저장되며,
+ * 모든 정적 파일에 대한 접근을 직접 허용하지 않고, 해당 클래스를 통해 간접적으로 접근할 수 있음.
+ *
+ * @since 2023-05-27
+ * @author 왕해삼(@kkkkkksssssaaaa)
+ * */
 export class ResourceHandler {
   private static readonly pathOfForbiddenWordFile: string =
     '../resources/forbidden-words.csv';
