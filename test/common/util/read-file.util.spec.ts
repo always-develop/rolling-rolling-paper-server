405d05a81e428b7d9d5284bd252133d90c0413d3
kkkkkksssssaaaa <kofc312@naver.com>
2023-05-31 23:29:31 +0900

    test: modify import path by domain package

--- test/common/util/read-file.util.spec.ts
+++ test/common/util/read-file.util.spec.ts
@@ -1,6 +1,6 @@
 import { convertStaticCsvFileToObjectArray } from 'src/common/utils/read-file.util';
-import { ForbiddenWordMetaData } from 'src/forbidden-words/forbidden-word.metadata';
-import { ForbiddenWord } from 'src/forbidden-words/forbidden-word.type';
+import { ForbiddenWordMetaData } from 'src/domain/forbidden-words/forbidden-word.metadata';
+import { ForbiddenWord } from 'src/domain/forbidden-words/forbidden-word.type';
 
 describe('message-validator.printForbiddenWords', () => {
   test('금칙어 파일을 읽을 때 예외가 발생하지 않는다.', () => {
