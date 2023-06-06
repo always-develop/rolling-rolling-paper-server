70cecceede4dce633e046ad7b7795cb511a1b3e5
kkkkkksssssaaaa <kofc312@naver.com>
2023-06-06 23:08:45 +0900

    refactor: make singleton instance to static forbidden word repository

--- src/domain/forbidden-words/static-forbidden-word.repository.ts
+++ src/domain/forbidden-words/static-forbidden-word.repository.ts
@@ -3,7 +3,15 @@ import { ForbiddenWords } from './forbidden-words.entity';
 import { ForbiddenWord } from './forbidden-word.type';
 
 export class StaticForbiddenWordRepository implements ForbiddenWordRepository {
+  private static readonly singletonInstance = new StaticForbiddenWordRepository(
+    ForbiddenWords.initializeStaticFiels(),
+  );
+
   constructor(private readonly store: ForbiddenWords) {}
+
+  public static getInstance(): ForbiddenWordRepository {
+    return StaticForbiddenWordRepository.singletonInstance;
+  }
 
   public findAll(): ForbiddenWords {
     return this.store;
