52d95c30d3eda65f835f1795485763a80170c269
kkkkkksssssaaaa <kofc312@naver.com>
2023-06-07 22:21:38 +0900

    feat: add message validation util

--- src/common/utils/message-validator.util.ts
+++ src/common/utils/message-validator.util.ts
@@ -1,1 +1,20 @@
-export class MessageValidator {}
+import { ForbiddenWordRepository } from 'src/domain/forbidden-words/forbidden-word.repository';
+import { StaticForbiddenWordRepository } from '../../domain/forbidden-words/static-forbidden-word.repository';
+import { MessageValidationError } from '../exceptions/message-validation.error';
+export class MessageValidator {
+  private readonly repository: ForbiddenWordRepository;
+
+  private constructor(repository: ForbiddenWordRepository) {
+    this.repository = repository;
+  }
+
+  public static check(): MessageValidator {
+    return new MessageValidator(StaticForbiddenWordRepository.getInstance());
+  }
+
+  public containForbiddenWord(message: string): void {
+    if (this.repository.findAll().includedInThisText(message)) {
+      throw new MessageValidationError();
+    }
+  }
+}
