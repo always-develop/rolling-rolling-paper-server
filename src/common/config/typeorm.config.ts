ccbfcfb2503328304d46b1e24e59dac81b15c516
kkkkkksssssaaaa <kofc312@naver.com>
2023-06-11 00:04:40 +0900

    feat: add typeorm configuration variable

---
+++ src/common/config/typeorm.config.ts
@@ -0,0 +1,13 @@
+import { TypeOrmModuleOptions } from '@nestjs/typeorm';
+
+export const typeORMConfig: TypeOrmModuleOptions = {
+  type: 'postgres',
+  host: process.env.DB_HOST,
+  port: Number(process.env.PORT),
+  username: process.env.DB_USERNAME,
+  password: process.env.DB_PASSWORD,
+  database: process.env.DB_NAME,
+  // import 할 entity file 정의
+  entities: [__dirname + '/../**/*.entity.{js,ts}'],
+  synchronize: true,
+};
