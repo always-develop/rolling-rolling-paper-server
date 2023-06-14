e15c017dac9f109bdaab5672d1a53216252cb8b7
kkkkkksssssaaaa <kofc312@naver.com>
2023-06-14 23:09:34 +0900

    chore: set environment profile

--- src/common/config/typeorm.config.ts
+++ src/common/config/typeorm.config.ts
@@ -4,7 +4,7 @@ export const typeORMConfig: TypeOrmModuleOptions = {
   type: 'postgres',
   host: process.env.DB_HOST,
   port: Number(process.env.PORT),
-  username: process.env.DB_USERNAME,
+  username: process.env.DB_USER,
   password: process.env.DB_PASSWORD,
   database: process.env.DB_NAME,
   // import 할 entity file 정의
