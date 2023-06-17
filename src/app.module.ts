2e128ed652d622a2ae888f9fcfda5e050413d1be
kkkkkksssssaaaa <kofc312@naver.com>
2023-06-18 00:47:24 +0900

    feat: add global import to typeorm

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,11 +1,15 @@
 import { Module } from '@nestjs/common';
 import { ConfigModule } from '@nestjs/config';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { typeORMConfig } from './common/config/typeorm.config';
 
 @Module({
   imports: [
     ConfigModule.forRoot({
       cache: true,
+      isGlobal: true,
     }),
+    TypeOrmModule.forRoot(typeORMConfig),
   ],
   controllers: [],
   providers: [],
