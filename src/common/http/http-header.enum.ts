58a1d9d9f3847753702fbf3a8fdf017b83e5a8b4
kkkkkksssssaaaa <kofc312@naver.com>
2023-07-02 01:25:50 +0900

    feat: add enum of http header key

---
+++ src/common/http/http-header.enum.ts
@@ -0,0 +1,10 @@
+/**
+ * HTTP Request Header 에 사용되는 상수 정의
+ *
+ * @sence 2023-07-02
+ * @author 왕해삼(@kkkkkksssssaaaa)
+ */
+export const enum HttpHeaderKey {
+  APPLICATION_JSON = 'application/json',
+  AUTHORIZATION = 'Authorization',
+}
