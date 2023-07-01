0f38be1cf48efb0b1a3709ad85cf05d4466381dd
kkkkkksssssaaaa <kofc312@naver.com>
2023-07-02 00:35:59 +0900

    feat: add custom http property factory

---
+++ src/common/http/custom-http.factory.ts
@@ -0,0 +1,15 @@
+import { HttpModuleOptions, HttpModuleAsyncOptions } from '@nestjs/axios';
+import { Injectable } from '@nestjs/common';
+import { ConfigService } from '@nestjs/config';
+
+@Injectable()
+export class HttpConfig implements HttpModuleAsyncOptions {
+  private constructor(private readonly systemProperty: ConfigService) {}
+
+  public useFactory(): HttpModuleOptions {
+    return {
+      timeout: this.systemProperty.get('HTTP_CONNECTION_TIMEOUT'),
+      maxRedirects: this.systemProperty.get('HTTP_MAX_REDIRECTS'),
+    };
+  }
+}
