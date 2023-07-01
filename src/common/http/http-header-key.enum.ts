c0f14a224104e357e0290f65dbe544c92ba76e29
kkkkkksssssaaaa <kofc312@naver.com>
2023-07-02 01:38:23 +0900

    refactor: extract enum header key and value

--- src/common/http/http-header-key.enum.ts
+++ src/common/http/http-header-key.enum.ts
@@ -1,10 +1,10 @@
 /**
- * HTTP Request Header 에 사용되는 상수 정의
+ * HTTP Request Header 에 사용되는 키 값에 대한 상수 정의
  *
  * @sence 2023-07-02
  * @author 왕해삼(@kkkkkksssssaaaa)
  */
 export const enum HttpHeaderKey {
-  APPLICATION_JSON = 'application/json',
+  CONTENT_TYPE = 'Content-Type',
   AUTHORIZATION = 'Authorization',
 }
---
+++ src/common/http/http-header-value.enum.ts
@@ -0,0 +1,9 @@
+/**
+ * HTTP Request Header 의 키에 사용되는 값 유형에 대한 상수 정의
+ *
+ * @sence 2023-07-02
+ * @author 왕해삼(@kkkkkksssssaaaa)
+ */
+export const enum HttpHeaderValue {
+  APPLICATION_JSON = 'application/json',
+}
