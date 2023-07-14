381e9878d4bc2f655e778a02dfd39e34675e59de
kkkkkksssssaaaa <kofc312@naver.com>
2023-07-14 18:55:14 +0900

    refactor: change class declare

--- src/common/http/http-request.model.ts
+++ src/common/http/http-request.model.ts
@@ -3,44 +3,17 @@ import { HttpHeader } from './http-header.model';
 import { QueryParameter } from './query-parameter.model';
 
 export class HttpRequest {
-  private _header?: HttpHeader;
-  private _queryParameter?: QueryParameter;
-  private _bodyParameter?: BodyParameter;
-
-  constructor(private readonly url: string) {}
-
-  public static to(url: string): HttpRequest {
-    return new HttpRequest(url);
-  }
-
-  public getUrl(): string {
-    return this.url;
-  }
-
-  public getHeader(): HttpHeader {
-    return this._header === null ? HttpHeader.default() : this._header;
-  }
-
-  public getQueryParameter(): QueryParameter {
-    return this._queryParameter;
-  }
-
-  public getBodyParameter(): BodyParameter {
-    return this._bodyParameter;
-  }
-
-  public header(header: HttpHeader): HttpRequest {
-    this._header = header;
-    return this;
-  }
-
-  public queryParameter(queryParameter: QueryParameter): HttpRequest {
-    this._queryParameter = queryParameter;
-    return this;
-  }
-
-  public bodyParameter(bodyParameter: BodyParameter): HttpRequest {
-    this._bodyParameter = bodyParameter;
-    return this;
+  constructor(
+    readonly url: string,
+    readonly header?: HttpHeader,
+    readonly queryParameter?: QueryParameter,
+    readonly bodyParameter?: BodyParameter,
+  ) {
+    this.url = url;
+    this.header = header === undefined ? HttpHeader.default() : header;
+    this.queryParameter =
+      queryParameter === undefined ? QueryParameter.default() : queryParameter;
+    this.bodyParameter =
+      bodyParameter === undefined ? BodyParameter.default() : bodyParameter;
   }
 }
