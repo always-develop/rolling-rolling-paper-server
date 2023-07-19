e215adf2bada0755665845c4e284ec77fffaea3e
kkkkkksssssaaaa <kofc312@naver.com>
2023-07-19 23:05:38 +0900

    feat: add kakao passport strategy

---
+++ src/common/authentication/kakao-auth.strategy.ts
@@ -0,0 +1,20 @@
+import { PassportStrategy } from '@nestjs/passport';
+import { Strategy } from 'passport-kakao';
+
+export class JwtKakaoStrategy extends PassportStrategy(Strategy, 'kakao') {
+  constructor() {
+    super({
+      clientID: process.env.OAUTH_KAKAO_CLIENT_ID,
+      clientSecret: process.env.OAUTH_KAKAO_CLIENT_SECRET,
+      callbackURL: process.env.OAUTH_KAKAO_REDIRECT,
+      scope: ['profile_nickname'],
+    });
+  }
+
+  async validate(accessToken: string, refreshToken: string, profile: any) {
+    // TODO: define do something...
+    return {
+      name: profile.displayName,
+    };
+  }
+}
