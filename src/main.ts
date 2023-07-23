39c58dd1e5428cf385bd9302131e31c3553e191f
kkkkkksssssaaaa <kofc312@naver.com>
2023-07-23 19:18:29 +0900

    chore: set global logger level

--- src/main.ts
+++ src/main.ts
@@ -2,7 +2,9 @@ import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 
 async function bootstrap() {
-  const app = await NestFactory.create(AppModule);
+  const app = await NestFactory.create(AppModule, {
+    logger: ['error', 'debug', 'log', 'warn'],
+  });
   await app.listen(3000);
 }
 bootstrap();
