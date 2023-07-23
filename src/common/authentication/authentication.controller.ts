47513ca93bd65fad23078fd258daad36cb3186a9
kkkkkksssssaaaa <kofc312@naver.com>
2023-07-23 19:20:22 +0900

    feat: add kakao login api and callback api

--- src/common/authentication/authentication.controller.ts
+++ src/common/authentication/authentication.controller.ts
@@ -1,15 +1,20 @@
 import { Controller, Get, Req, UseGuards } from '@nestjs/common';
 import { AuthGuard } from '@nestjs/passport';
 import { AuthenticationService } from './authentication.service';
+import { Request } from 'express';
 
 @Controller('/auth')
 export class AuthenticationController {
   constructor(private readonly service: AuthenticationService) {}
 
   @UseGuards(AuthGuard('kakao'))
-  @Get('/login/kakao')
-  public doLoginByKakao(@Req() req) {
+  @Get('/kakao')
+  public getAuthorizationFromKakao(@Req() req: Request) {
     console.log(req);
-    return req;
+  }
+
+  @Get('/kakao/login')
+  public kakaoLoginCallBack(@Req() code: string) {
+    return this.service.getTokenByCode(code);
   }
 }
